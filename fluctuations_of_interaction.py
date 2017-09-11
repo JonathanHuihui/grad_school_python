4bdc13597998dd6efeb710d1f11d0897d2e8275c
Jonathan Huihui <30911328+JonathanHuihui@users.noreply.github.com>
2017-09-11 12:15:14 -0600

    Add files via upload

--- fluctuations_of_interaction.py
+++ fluctuations_of_interaction.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Determine the fluctuations of energy 
 # Trying to make this generalizable from the input of a "protein_info.txt" file
 # that should contain the names of all the proteins. 
@@ -7,9 +6,12 @@ import numpy as np
 import pandas as pd
 import sys 
 import math
+from scipy import stats
+import matplotlib.pyplot as plt
 
 # Import the "protein_info.txt" file for name information 
-protInfo_filename = sys.argv[1]
+# protInfo_filename = sys.argv[1]
+protInfo_filename = 'protein_info.txt'
 protInfo_file = pd.read_table(protInfo_filename, header = None , delim_whitespace=True)
 protnames = protInfo_file.iloc[:,0]
 
@@ -17,84 +19,83 @@ protnames = protInfo_file.iloc[:,0]
 subset = 'folded_state_fifty_clusters'
 
 # Initialize number of clusters
-# numclusters_all = 50
+numclusters_all = 50
 
 # Could also determine number of clusters from a column in "protein_info.txt"
-numclusters_all = protInfo_file.iloc[:, 3]
+# numclusters_all = protInfo_file.iloc[:,INSERT COLUMN NUMBER HERE DUMMY]
 
 # Determine number of proteins
 numprots = len(protnames)
-
 # Initialize an ouput table
 export = np.empty(0)
 
 # This code computes the variation of the energy values
-for iter in range(0, numprots):
-    stdDev = 0.0
+for iter in xrange(0, numprots, 2):
 # in case the number of clusters is not constant
-    numclusters = numclusters_all[iter]   
-# numclusters = 50
-    cluster_weights_file_name = protnames[iter] + '_50_cluster_summary.dat'
-    cluster_weights_file = pd.read_table(cluster_weights_file_name,
+# numclusters = num_clusters_all[iter]
+    prot_family = protnames[iter].split('_')[0]
+    uint_array_1 = np.empty(0)
+    uint_array_2 = np.empty(0)
+    weights_array_1 = np.empty(0)
+    weights_array_2 = np.empty(0)
+    for pair_num in range(0, 2):
+        numclusters = 50
+        cluster_weights_file_name = protnames[iter + pair_num] + '_cluster_summary.dat'
+        cluster_weights_file = pd.read_table(cluster_weights_file_name,
                                          delim_whitespace=True)
-    weights = cluster_weights_file.iloc[0:numclusters, 2] 
-    uint = np.empty(0)                     
-    for z in range(0, numclusters):
-        solvation_file_name = protnames[iter] + '_c' + str(z) + '.txt'
-        solvation_file = pd.read_table(solvation_file_name,
+        weights = cluster_weights_file.iloc[:,2] 
+        uint = np.empty(0)
+        cut_weights = np.empty(0)
+        stdDev = 0.0                    
+        for z in range(0, numclusters):
+            solvation_file_name = protnames[iter + pair_num] + '_c' + str(z) + '.txt'
+            solvation_file = pd.read_table(solvation_file_name,
                                          delim_whitespace=True,
                                          header = None)
-        solv = np.sum(solvation_file.iloc[:,1])
-        allOnTerms_file_name = 'allOnTerms_' + protnames[iter] +'_c' + str(z) + '.txt'
-        allOnTerms_file = pd.read_table(allOnTerms_file_name,
+            solv = np.sum(solvation_file.iloc[:,1])
+            allOnTerms_file_name = 'allOnTerms_' + protnames[iter + pair_num] +'_c' + str(z) + '.txt'
+            allOnTerms_file = pd.read_table(allOnTerms_file_name,
                                          delim_whitespace=True,
                                          header = None)
-        ucoul = allOnTerms_file.iloc[0,2]
-        crfe = allOnTerms_file.iloc[0,0]
-        upol = crfe - solv
-        uint = np.append(uint, upol + ucoul)
-    average = np.average(uint, weights=weights)
-    variance = np.average((uint-average)**2, weights=weights) 
-    stdDev = math.sqrt(variance)
-#    outputFileName  = protnames[iter] + '_variation.txt'
-    print uint 
+            ucoul = allOnTerms_file.iloc[0,2]
+            crfe = allOnTerms_file.iloc[0,0]
+            upol = crfe - solv
+            uint = np.append(uint, upol + ucoul)
+            #if weights[z] > 0:
+            #    uint = np.append(uint, upol + ucoul)
+            #    cut_weights = np.append(cut_weights, weights[z])
+        if pair_num == 0:
+            uint_array_1 = uint
+            weights_array_1 = weights
+        elif pair_num == 1:
+            uint_array_2 = uint
+            weights_array_2 = weights
+#        average = np.average(uint, weights=weights)
+#        variance = np.average((uint-average)**2, weights=weights) 
+#        stdDev = math.sqrt(variance)
+    uint_array_1 = np.multiply(uint_array_1, weights_array_1)
+    uint_array_2 = np.multiply(uint_array_2, weights_array_2)
+    pval = stats.wilcoxon(uint_array_1, uint_array_2)
+#    outputFileName  = prot_family + '_pval.txt'
 #    f = open(outputFileName, 'w')
-#    f.write(str(stdDev))
-#    f.close
-    
+#    f.write(str(pval))
+#    f.close       
+    plt.scatter(uint_array_1, weights_array_1, color='blue')
+    plt.scatter(uint_array_2, weights_array_2, color='red')
+    plt.xlabel('Uint')
+    plt.ylabel('Weights')
+    plt.title(protnames[iter] + ' ' + protnames[iter + pair_num] + ' folded energy distributions')
+    plt.show
+    plt.savefig(protnames[iter] + '_' + protnames[iter + pair_num] + '_energy_distributions.pdf' )
+    print protnames[iter]
+    print protnames[iter + pair_num]
+    print pval
+#    print norm_test_1
+#    print norm_test_2
 
 
 
 
 
-"""#This code reproduces the output values from the similar Mathematica code
-for iter in range(0, numprots):
-# in case the number of clusters is not constant
-# numclusters = num_clusters_all[iter]   
-    numclusters = 50
-    cluster_weights_file_name = protnames[iter] + '_cluster_summary.dat'
-    cluster_weights_file = pd.read_table(cluster_weights_file_name,
-                                         delim_whitespace=True)
-    cluster_weights = cluster_weights_file.iloc[:,2] 
-    uint = np.empty(0)                     
-    for z in range(0, numclusters):
-        solvation_file_name = protnames[iter] + '_c' + str(z) + '.txt'
-        solvation_file = pd.read_table(solvation_file_name,
-                                         delim_whitespace=True,
-                                         header = None)
-        solv = np.sum(solvation_file.iloc[:,1])
-        allOnTerms_file_name = 'allOnTerms_' + protnames[iter] +'_c' + str(z) + '.txt'
-        allOnTerms_file = pd.read_table(allOnTerms_file_name,
-                                         delim_whitespace=True,
-                                         header = None)
-        weight = cluster_weights[z]
-        ucoul = allOnTerms_file.iloc[0,2]
-        crfe = allOnTerms_file.iloc[0,0]
-        upol = crfe - solv
-        uint += (upol + ucoul)*weight
-    export = np.append(export, uint)
 
 
-outputfilename = subset + '_energy_results.txt'
-np.savetxt(outputfilename, export, fmt="%.4f")
-"""
